16d07bd0493dcfe3c8a34e986295856e3a3884d1
ljbeal <louis.j.beal@gmail.com>
2023-02-16 10:13:38 +0100

    [tests] update test to be a clone of the example

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,37 +1,6 @@
 """ Tests for calculations."""
-import os
-
-from aiida.engine import run
-from aiida.orm import SinglefileData
-from aiida.plugins import CalculationFactory, DataFactory
-
-from . import TEST_DIR
+from examples.example_01 import test_run
 
 
 def test_process(bigdft_new_code):
-    """Test running a calculation
-    note this does not test that the expected outputs are created of output parsing"""
-
-    # Prepare input parameters
-    DiffParameters = DataFactory("bigdft_new")
-    parameters = DiffParameters({"ignore-case": True})
-
-    file1 = SinglefileData(file=os.path.join(TEST_DIR, "input_files", "file1.txt"))
-    file2 = SinglefileData(file=os.path.join(TEST_DIR, "input_files", "file2.txt"))
-
-    # set up calculation
-    inputs = {
-        "code": bigdft_new_code,
-        "parameters": parameters,
-        "file1": file1,
-        "file2": file2,
-        "metadata": {
-            "options": {"max_wallclock_seconds": 30},
-        },
-    }
-
-    result = run(CalculationFactory("bigdft_new"), **inputs)
-    computed_diff = result["bigdft_new"].get_content()
-
-    assert "content1" in computed_diff
-    assert "content2" in computed_diff
+    test_run(bigdft_new_code)
